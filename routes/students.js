b0aa87baf6cd2bb7674eadc831a5df1b2153aa5f
Akash Raveendran <akashraveendran@gmail.com>
2022-04-09 16:55:15 +0530

    completed the student module draft 1

--- routes/students.js
+++ routes/students.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 //CONTROLLER
-const { getSignupPage, doSignup } = require("../controllers/student-controller")
+const { getSignupPage, doSignup, getLoginPage, doLogin, getHomePage, logout, searchLibrary, searchColleges } = require("../controllers/student-controller")
+
+//middleware for authentication
+const checkStudent = require('../middleware/checkStudent')
 
 
 
-
-router.route('/');
+router.route('/').get(getHomePage);
 router.route('/signup').get(getSignupPage).post(doSignup);
-router.route('/login');
-router.route('/logout');
-router.route('/search-libraries');
-router.route('/search-Colleges');
-router.route('/search-hospitals');
+router.route('/login').get(getLoginPage).post(doLogin);
+router.route('/logout').get(checkStudent, logout);
+router.route('/search-libraries').get(checkStudent, searchLibrary);
+router.route('/search-Colleges').get(checkStudent, searchColleges);
 router.route("/view-map");
 
 
